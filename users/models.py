cead2c642286b580af83a924a43d2ba6abc9a6e8
LuisHenrique01 <luizbueno1201@gmail.com>
2021-10-21 00:32:53 -0300

    tentativa de criar um model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Proprietario(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='proprietario')
+    cpf = models.CharField('CPF', max_length=20)
+    
+
+    class Meta:
+        verbose_name = "Proprietário"
+        verbose_name_plural = "Proprietários"
+
+    def __str__(self):
+        return self.user.first_name
