6621a60bf9dd2f0c80d7c78dc5df90a3a9339507
LuisHenrique01 <luizbueno1201@gmail.com>
2021-10-21 19:26:49 -0300

    Swagger inital

--- beauty/urls.py
+++ beauty/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('user/', include('users.urls')),
     path('estabelecimento/', include('estabelecimentos.urls')),
