86ee1455f16e4497d2431baa3b7776763b0ba18d
LuisHenrique01 <luizbueno1201@gmail.com>
2021-12-01 22:55:06 -0300

    add admin app

--- beauty/settings.py
+++ beauty/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['beauty-2.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    #    'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
