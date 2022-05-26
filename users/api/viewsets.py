73f6d5861a07f7a2e610c009e02b4da4f1205c6d
hilton <hilton@email.com>
2022-05-26 03:07:14 -0300

    return token

--- users/api/viewsets.py
+++ users/api/viewsets.py
@@ -1,16 +1,33 @@
 from rest_framework import generics, permissions
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from users.models import Proprietario
 from .serializers import UserCreateSerializer, UserDetailSerializer, UserChangePasswordSerializer, ProprietarioSerializer
 
 
 class UserCreate(generics.CreateAPIView):
-    queryset = User.objects.all()
     serializer_class = UserCreateSerializer
     permission_classes = [permissions.AllowAny]
+
+    def get_user(self, data):
+        return User.objects.get(username=data['username'])
+
+    def get_tokens_for_user(self, user):
+        refresh = RefreshToken.for_user(user)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        data_token = self.get_tokens_for_user(self.get_user(serializer.data))
+        return Response(data_token, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class UserChangePassword(generics.UpdateAPIView):
