e03c65a7d7f3a839183f6b0e15c369d73c2d9c9f
LuisHenrique01 <luizbueno1201@gmail.com>
2022-05-26 20:25:39 -0300

    uninstall drf

--- estabelecimentos/urls.py
+++ estabelecimentos/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 
-from estabelecimentos.views import Docs
 from .api.viewsets import AgendaList, AgendaList, EstabelecimentoViewSet, HorarioCreate, HorarioDetail, ServicoViewSet
 from rest_framework import routers
 
@@ -9,7 +8,6 @@ router.register('estabelecimento', EstabelecimentoViewSet)
 router.register('servico', ServicoViewSet)
 
 urlpatterns = [
-    path("docs/", Docs.as_view(), name="dosc"),
     path("", include(router.urls)),
     path("horario/", HorarioCreate.as_view()),
     path("agenda/", AgendaList.as_view()),
