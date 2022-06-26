d2d6f1100908ec25102879274f9b9692c69e2e58
LuisHenrique01 <luizbueno1201@gmail.com>
2022-06-26 14:41:42 -0300

    test ci

--- beauty/settings.py
+++ beauty/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # Para não precisar exportar toda hora a secret key
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
+print(len(SECRET_KEY))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
